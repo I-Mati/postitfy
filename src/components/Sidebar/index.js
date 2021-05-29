c211c5145bb2347c057764fcb25aa3a08a00d86f
Matias Iglesias <iglesiasmatiasa1@gmail.com>
2021-05-29 12:06:28 -0300

    Add Trash Bin Icon and logic to show it

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ColorPicker from "../ColorPicker";
+import emptyBin from "../../assets/empty-bin.png";
+import fullBin from "../../assets/full-bin.png";
 import "./style.css";
 
-const Sidebar = () => (
+const Sidebar = ({ isTrashEmpty = true }) => (
   <div id="sidebar">
-    <h1 id="logo">Postitfy</h1>
-    <ColorPicker />
+    <div id="elementContainer">
+      <h1 id="logo">Postitfy</h1>
+      <ColorPicker />
+    </div>
+    <img id="trash" src={isTrashEmpty ? emptyBin : fullBin} alt="trash icon" />
   </div>
 );
+
+Sidebar.defaultValues = {
+  isTrashEmpty: true,
+};
+
+Sidebar.propTypes = {
+  isTrashEmpty: PropTypes.number.isRequired,
+};
 
 export default Sidebar;
