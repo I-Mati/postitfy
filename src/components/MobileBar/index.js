930b6b1dbd897001db1d9b6a46c57c67b090b258
Matias Iglesias <iglesiasmatiasa1@gmail.com>
2021-06-12 10:30:57 -0300

    Delete unused var

--- src/components/MobileBar/index.js
+++ src/components/MobileBar/index.js
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faTrashAlt,
   faTimesCircle,
   faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
